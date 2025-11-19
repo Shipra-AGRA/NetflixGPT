61a9753c20e9115199f55fa5cbe4684df6873b72
Shipra-AGRA <shipraagrawal230@gmail.com>
2025-11-19 17:13:42 +0530

    secure keys in .env file

--- src/utils/const.js
+++ src/utils/const.js
@@ -2,10 +2,10 @@ export const API_OPTIONS={
     Method:"GET",
     headers:{
         "Content-Type":"application/JSON",
-        "Authorization":"Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NTVjZmU4ZTczNTlhM2E2ZjBlMTc4NmMxN2E4YmRiMCIsIm5iZiI6MTc2MzM1NTM3My40MzgwMDAyLCJzdWIiOiI2OTFhYWFlZDlmZGY3NTUyYWE1ZGZkNGUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.UH4ucS-HFUKk_5h05Xtlr3Hepd1BX7RXGZ9DAL-Pjao"
+        "Authorization":"Bearer "+process.env.REACT_APP_TMDB_KEY
     }
 }
 
 export const IMG_CDN="https://image.tmdb.org/t/p/w780"
 
-export const OPENAI_KEY="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"
+export const OPENAI_KEY=process.env.REACT_APP_OPENAI_KEY
