26c9e50e94ac30d253dc2e276fdfd6c1503b493b
ShipraAgrawal230 <shipraagrawal230@gmail.com>
2026-02-01 23:25:45 +0530

    updated app.js

--- src/App.js
+++ src/App.js
@@ -14,3 +14,33 @@ export default App
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(<App />)
+
+
+// const App = () => {
+//     return (
+//         <Provider store={appStore}>
+//             <Outlet />
+//         </Provider>
+//     )
+// }
+// export default App
+
+//  const appRouter = createBrowserRouter([
+//     {
+//         path:'/',
+//     element:<App/>,
+//     children:[
+//         {
+//         path: "/",
+//         element: <Login />
+//     },
+//     {
+//         path: "/browse",
+//         element: <Browse />
+//     }
+//     ]
+//     }
+// ])
+
+// const root = ReactDOM.createRoot(document.getElementById("root"))
+// root.render(<RouterProvider router={appRouter} />)
